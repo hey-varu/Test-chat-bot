90c6f921a5b380fe5a7344779a0685ddaca50783
hey-varu <168971394+hey-varu@users.noreply.github.com>
2024-06-12 16:44:43 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+import discord 
+import responses
+
+TOKEN = 'MTI1MDM4NjcyOTIzNjU2MTk3MQ.GY1cP0.2Ig80ZCbu2Jc1vgO5jIPTB-88yA4NK7jbMZMJM'
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message.startswith('?'):
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
+
+# Outside the function
+run_discord_bot()
+
+
+
+
+    
